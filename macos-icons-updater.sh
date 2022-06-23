db526d0235819a1240821c4c1e84067a0984ef7e
afwolfe <3718652+afwolfe@users.noreply.github.com>
2022-06-23 19:59:21 -0400

    Add additional arguments, support different icon and app folders

--- macos-icons-updater.sh
+++ macos-icons-updater.sh
@@ -1,54 +1,79 @@
+#!/bin/sh
 # Get current directory
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-# Check -h flag
-if [[ "$1" = "-h" ]]
-then
-    echo "===============================================================\n\033[0;32mmacOS Icons Updater\033[0m \nhttps://github.com/vchkhr/macos-icons-updater \nPlease read the documentation from the link above before using. \nMIT License is attached at the link above. \n==============================================================="
+help() {
+    echo "==============================================================="
+    echo "\033[0;32mmacOS Icons Updater\033[0m"
+    echo "https://github.com/vchkhr/macos-icons-updater"
+    echo "Please read the documentation from the link above before using."
+    echo "MIT License is attached at the link above."
+    echo "==============================================================="
+    echo " -h   --help              print this help message"
+    echo " -s   --no-sudo           run script without sudo"
+    echo " -q   --quiet             reduce output"
+    echo " -i   --icons <PATH>      set the path to the icons. Default to current directory."
+    echo " -a   --apps  <PATH>      set the path to the .app bundles. Default to /Applications"
+    exit 0
+}
 
-    return
-fi
-
-# Check -nosudo flag
+#default values
 no_sudo=0
-if [[ "$1" = "-nosudo" ]]
-then
-    no_sudo=1
+icon_dir="$script_dir"
+app_dir="/Applications"
+# handle multiple arguments
+while [ "$1" != "" ]; do
+    case "$1" in
+        -h | --help ) help; shift ;;
+        -s | --no-sudo ) no_sudo=1 ; shift ;;
+        -q | --quiet ) quiet=1; shift;;
+        -i | --icons ) icon_dir="$2"; shift 2;;
+        -a | --apps ) app_dir="$2"; shift 2;;
+        * ) shift ;; # shift past unknown args
+    esac
+done
+
+# Verify directories exist before continuing
+if [ ! -d $icon_dir ]; then
+    echo "Icon directory: $icon_dir does not exist"
+    exit 1
 fi
+
+if [ ! -d $app_dir ]; then
+    echo "App directory: $app_dir does not exist"
+    exit 1
+fi
+
 
 # Count successful and failed jobs
 successful_jobs=0
 failed_jobs=0
     
 # Process all files in the current directory
-for icon in *;
-do
-    # Do not process files of this repository
-    if [ $icon = "LICENSE" ] || [ $icon = "macos-icons-updater.sh" ] || [ $icon = "moiu-logo.png" ] || [ $icon = "README.md" ]
-    then
-        continue
-    fi
-
-    # Check if file is icon
-    if ! [ ${icon: -5} = ".icns" ]
-    then
-        echo "\033[1;33mFile was ignored because it is not an icon: "$icon"\033[0m"
-        continue
-    fi
-
+# https://unix.stackexchange.com/questions/9496/looping-through-files-with-spaces-in-the-names#9499
+# Loops over files ending in .icns with spaces without a subshell.
+IFS='
+'
+set -f
+for iconpath in $(find $icon_dir -name "*.icns"); do
+    # Shorten icon to just the filename
+    icon=$(basename "$iconpath")
     # Get app`s name from icon`s name
     app=${icon/".icns"/""}
+    app_contents="$app_dir/"$app".app/Contents"
 
     # Check if there are app and it's "Info.plist" file
-    if [ ! -f /Applications/"$app".app/Contents/Info.plist ]
+    if [ ! -f "$app_contents/Info.plist" ]
     then
-        echo -e "\033[0;31mNo app found: "$app"\033[0m"
+        if [[ $quiet -eq 0 ]]; then
+            echo "\033[0;31mNo app found: "$app"\033[0m"
+        fi
         (( failed_jobs++ ))
         continue
     fi
 
     # Get the "Info.plist" file of the app (icon's name is written there)
-    info_plist="/Applications/"$app".app/Contents/Info.plist"
+    info_plist="$app_contents/Info.plist"
     nextLineIsAppIcon=0
     appIcon=""
 
@@ -75,7 +100,9 @@ do
     # Check if icon's name is not found
     if [[ $nextLineIsAppIcon = 0 ]]
     then
-        echo -e "\033[0;31mUnable to find icon name in \"Info.plist\" for this app: "$app"\033[0m"
+        if [[ $quiet -eq 0 ]]; then
+            echo "\033[0;31mUnable to find icon name in \"Info.plist\" for this app: "$app"\033[0m"
+        fi
         (( failed_jobs++ ))
         continue
     fi
@@ -89,21 +116,23 @@ do
     # Replace app's standard icon with the new one and count successful and failed jobs
     if [[ $no_sudo = 0 ]]
     then
-        sudo cp ""$script_dir"/"$icon"" "/Applications/"$app".app/Contents/Resources/"$appIcon""
-        (( successful_jobs++ ))
+        sudo cp "$iconpath" "$app_contents/Resources/$appIcon"
+        ((successful_jobs++))
     else
         # Check if file is writable or not
-        if [ -w "/Applications/"$app".app/Contents/Resources/"$appIcon ]
+        if [ -w "$app_contents/Resources/$appIcon" ]
         then
-            cp ""$script_dir"/"$icon"" "/Applications/"$app".app/Contents/Resources/"$appIcon""
-            (( successful_jobs++ ))
+            cp "$iconpath" "$app_contents/Resources/$appIcon"
+            ((successful_jobs++))
         else
-            echo "\033[0;31mUnable to update icon for this application due to not writable permission: "$app"\033[0m"
-            (( failed_jobs++ ))
+            if [[ $quiet -eq 0 ]]; then
+                echo "\033[0;31mUnable to update icon for this application due to not writable permission: "$app"\033[0m"
+            fi
+            ((failed_jobs++))
         fi
     fi
-
 done
+# done < <(find $icon_dir -name "*.icns" -print0)
 
 # Reload Finder and Dock if not -nosudo flag
 if [[ $no_sudo = 0 ]]
